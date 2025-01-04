e4627c4aeaaa39d97f33e7e235a98e0c1f6a824e
PatShen <swxpat@qq.com>
2025-01-04 16:13:55 +0800

    Use TCA

--- TCADemo/TCADemo/TCADemoApp.swift
+++ TCADemo/TCADemo/TCADemoApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TCADemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(
+                store: Store(
+                    initialState: Counter(),
+                    reducer: counterReducer,
+                    environment: CounterEnvironment()
+                )
+            )
         }
     }
 }
